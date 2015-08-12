0289705d2217c72479fb70ddb330b610f6041f7d
Cesar Reyes <jcesarrc@gmail.com>
2015-08-12 22:18:57 +0000

    OK

--- script.r
+++ script.r
@@ -5,10 +5,10 @@ DIMX = dim(data)[1];
 DIMY = dim(data)[2];
 
 param <- unlist(strsplit(input," "));
-x0 = as.numeric(param[0]);
-x1 = as.numeric(param[1]);
-y0 = as.numeric(param[2]);
-y1 = as.numeric(param[3]);
+x0 = as.numeric(param[1]);
+x1 = as.numeric(param[2]);
+y0 = as.numeric(param[3]);
+y1 = as.numeric(param[4]);
 
 x = seq(x0,x1,length.out=DIMX);
 y = seq(y0,y1,length.out=DIMY);
