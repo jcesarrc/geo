d6ddf50c1a5b89ef1bc18d179821d8f44d0194dd
unknown <julio.reyes@GSIR3761.PARQUES.LOCAL>
2015-08-12 15:59:55 -0500

    1st

---
+++ script.r
@@ -0,0 +1,22 @@
+input <- readLines('geocol2004.txt', n=1);
+data <- read.table("geocol2004.txt", skip=1);
+
+DIMX = dim(data)[1];
+DIMY = dim(data)[2];
+
+param <- unlist(strsplit(input," "));
+x0 = as.numeric(param[0]);
+x1 = as.numeric(param[1]);
+y0 = as.numeric(param[2]);
+y1 = as.numeric(param[3]);
+
+x = seq(x0,x1,length.out=DIMX);
+y = seq(y0,y1,length.out=DIMY);
+
+m2D = merge(x,y);
+
+z = matrix(data, nrow=DIMX*DIMY, byrow=T);
+
+resultado = cbind(m2D,z);
+
+write.csv(resultado);
